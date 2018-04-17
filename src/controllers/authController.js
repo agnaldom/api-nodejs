5d71a340359025deeffc4c08a040c3c870d8aee8
agnaldom <agnaldoneto@ufpa.br>
2018-04-17 15:24:20 -0300

    Trocando send pelo status

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/register', async(req, res) => {
    try {
       if (await User.findOne({ email }))
-         return res.send(400).send({ error: 'User already exists' });
+         return res.status(400).send({ error: 'User already exists' });
 
       const user = await User.create(req.body);
 
