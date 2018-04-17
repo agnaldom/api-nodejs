8dd4e7313044d179dc3a295354286888a0e4fd5a
agnaldom <agnaldoneto@ufpa.br>
2018-04-17 15:27:41 -0300

    Pegando email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,8 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
+   const { email } = req.body;
+
    try {
       if (await User.findOne({ email }))
          return res.status(400).send({ error: 'User already exists' });
