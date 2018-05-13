3ce9cdee1560d8be29954ec10d9272735538eebc
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-13 16:18:04 -0300

    cria usuario, autentica e retorna token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,22 +2,32 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const authConfig = require('../config/auth');
+
 const User = require('../models/User');
 
 const router = express.Router();
 
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    });
+}
 router.post('/register', async (req, res) => {
    const { email } = req.body;
 
     try {
-      if (await User.findOne({ email }))
-         return res.status(400).send({ error: 'User already exists' });
+        if (await User.findOne({ email }))
+             return res.status(400).send({ error: 'User already exists' });
 
-      const user = await User.create(req.body);
+        const user = await User.create(req.body);
 
-      user.password = undefined;
+        user.password = undefined;
 
-      return res.send({ user });
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id }),
+        });
    } catch (err) {
          return res.status(400).send({ error: 'Registration failed' });
    }
@@ -34,11 +44,12 @@ router.post('/authenticate', async (req, res) => {
     if(!await bcrypt.compare(password, user.password))
         return res.status(400).send({ error: 'Invalid password' });
 
-    const token = jwt.sign({ id: user.id }, )
-
     user.password = undefined;
 
-    res.send({ user });
+    res.send({
+        user,
+        token: generateToken({ id: user.id }),
+    } );
 });
 
 module.exports = app => app.use('/auth', router);
