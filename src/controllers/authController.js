9c1ac237352fe19edeeddc776469e15027f33fb1
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-13 13:48:07 -0300

    Atualizando o projeto

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,11 +5,11 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
-   //const { email } = req.body;
+   const { email } = req.body;
 
-   try {
-      //if (await User.findOne({ email }))
-         //return res.status(400).send({ error: 'User already exists' });
+    try {
+      if (await User.findOne({ email }))
+         return res.status(400).send({ error: 'User already exists' });
 
       const user = await User.create(req.body);
 
