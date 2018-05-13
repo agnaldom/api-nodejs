e1a91acff8b8f71a01f6c367fae46f815e7ab7ca
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-13 20:11:28 -0300

    Definindo as rotas

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth');
 
+const Project = require('../models/Project');
+const Task = requite('../models/Task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId });
+router.get('/', async  (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.get('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.post('/', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.put('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.delete('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
 });
 
 module.exports = app => app.use('/projects', router);
